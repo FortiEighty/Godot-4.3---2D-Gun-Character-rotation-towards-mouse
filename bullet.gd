0b9c9eadfc54aa06b04cd7582acac8f620a1dd46
FortiEighty <deniss.akmaikin@gmail.com>
2024-11-10 16:53:11 +0200

    Files uploaded
    Files uploaded

---
+++ bullet.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# Speed of the bullet
+@export var speed : int = 500
+
+# Change position towards rotation
+func _process(delta: float) -> void:
+	position += transform.x * speed * delta
+
+# When object leaves the screen > delete it from tree
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
